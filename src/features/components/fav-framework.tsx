3552788b6c8d4e8a21c3a8149d96a25c6f5fbdcc
Ayan <ayanhasnain2572006@gmail.com>
2025-04-25 11:57:11 +0530

    fix

--- src/features/components/fav-framework.tsx
+++ src/features/components/fav-framework.tsx
@@ -1,3 +1,4 @@
+import { Heart } from "lucide-react";
 import React from "react";
 
 interface FavFrameWorkProps {
@@ -7,7 +8,8 @@ interface FavFrameWorkProps {
 
 export const FavFrameWork = ({ icon: Icon, title }: FavFrameWorkProps) => {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center relative">
+      <Heart className="size-4 absolute left-2 top-1"/>
       <Icon className="size-32" /> <h3 className="text-sm mt-2">{title}</h3>
     </div>
   );
